284c63ad8694aba7865c9a9267b5064bc59b8e61
raberana <raberana@live.com>
2012-12-28 22:29:25 +0800

    Fixed UI

---
+++ CafeProject/MainProject/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult OrderIndex()
+        {
+            return View();
+        }
+
+    }
+}
