c283f048abf22b3d9c31476e69a6d32f5e3408e8
raberana <raberana@live.com>
2012-12-30 15:34:09 +0800

    Fixed active url indicator

--- CafeProject/MainProject/Scripts/IndexCustom.js
+++ CafeProject/MainProject/Scripts/IndexCustom.js
@@ -1,13 +1,67 @@
-﻿window.onload = function swapPic() {
-    var imgSrc = [];
-    imgSrc[0] = "/Content/Resources/cafe1.jpg";
-    imgSrc[1] = "/Content/Resources/cafe2.jpg";
-    imgSrc[2] = "/Content/Resources/cafe3.jpg";
-    imgSrc[3] = "/Content/Resources/cafe4.jpg";
-    imgSrc[4] = "/Content/Resources/cafe5.jpg";
-    imgSrc[5] = "/Content/Resources/cafe6.jpg";
+﻿window.onload = function pageLoads() {
+    var url = document.URL;
+    var urlComponents = url.split("/");
+    
+    if (urlComponents.length <= 4)
+        swapPic();
+    else
+    {
+        switch (urlComponents[4])
+        {
+            case "AboutPage": setActiveUrlStyle("AboutPage");
+                break;
 
-    var randomnumber = Math.floor(Math.random() * 5);
+            case "FaqPage": setActiveUrlStyle("FaqPage");
+                break;
+
+            case "HoursLocationPage": setActiveUrlStyle("HoursLocationPage");
+                break;
+
+            case "MenuPage": setActiveUrlStyle("MenuPage");
+                break;
+
+            case "OrderIndex": setActiveUrlStyle("OrderIndex");
+                break;
+
+        }
+    }
+}
+
+function swapPic()
+{
+    document.getElementsByClassName("Menu")[0].style.paddingBottom = "20px";
+    var imgSrc = [];
+    imgSrc[0] = "Content/Resources/cafe1.jpg";
+    imgSrc[1] = "Content/Resources/cafe2.jpg";
+    imgSrc[2] = "Content/Resources/cafe3.jpg";
+    imgSrc[3] = "Content/Resources/cafe4.jpg";
+    imgSrc[4] = "Content/Resources/cafe5.jpg";
+    imgSrc[5] = "Content/Resources/cafe6.jpg";
+
+    var randomnumber = Math.floor(Math.random() * 6);
     var img = document.getElementById("imgContainer");
-    img.setAttribute("src", imgSrc[randomnumber]);
+    img.style.clear = "both";
+    img.style.minHeight = "470px";
+    img.style.minWidth = "950px";
+    img.style.backgroundImage = "url(" + imgSrc[randomnumber] + ")";
+}
+
+function setActiveUrlStyle(idContainer)
+{
+    var activeButton = document.getElementById(idContainer).style;
+    var buttons = document.getElementsByClassName("IndexListButtons");
+    
+    var ctr = 0;
+    for (ctr = 0; ctr < buttons.length; ctr++)
+    {
+        //buttons[ctr].removeAttribute("src");
+        buttons[ctr].className = "IndexListButtons"
+    }
+
+    activeButton.fontWeight = "bold";
+    activeButton.color="black";
+    activeButton.borderTop = "5px solid #5F9EA0";
+    activeButton.borderBottom = "20px solid #5F9EA0";
+    activeButton.margin = "-5px auto";
+    document.getElementsByClassName("Menu")[0].style.paddingBottom = "5px";
 }
